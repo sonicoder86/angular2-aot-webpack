5bd4dd7045722ff191d5f12e9e2a7ac41f181025
blacksonic <soos.gabor86@gmail.com>
2016-09-25 10:04:47 +0200

    fix webpack for prod

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -46,8 +46,11 @@ module.exports = {
   ],
 
   resolve: {
-    root: [ path.join(__dirname, 'app') ],
-    extensions: ['', '.ts', '.js']
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, 'app')
+    ],
+    extensions: ['.ts', '.js']
   },
 
   devtool: false
