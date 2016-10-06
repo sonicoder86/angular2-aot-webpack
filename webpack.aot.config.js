311e4c7a19e3a98d35d67d9ecbbaad874bcf1813
blacksonic <soos.gabor86@gmail.com>
2016-10-06 20:43:21 +0200

    trying to add webpack aot

---
+++ webpack.aot.config.js
@@ -0,0 +1,54 @@
+'use strict';
+let path = require('path');
+let webpack = require('webpack');
+let ngtools = require('@ngtools/webpack');
+
+module.exports = {
+  entry: {
+    'bootstrap.aot': './app/bootstrap.aot.ts',
+  },
+
+  output: {
+    path: './dist',
+    filename: '[name].bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          'awesome-typescript?tsconfig=tsconfig.json',
+          'angular2-template'
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: 'raw'
+      }
+    ]
+  },
+
+  plugins: [
+    new ngtools.NgcWebpackPlugin({
+      project: './tsconfig.aot.json',
+      baseDir: path.resolve(__dirname, '')
+    }),
+    new webpack.ProgressPlugin(),
+    new webpack.ContextReplacementPlugin(
+      // The (\\|\/) piece accounts for path separators in *nix and Windows
+      /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+      path.join(process.cwd(), 'app')
+    )
+  ],
+
+  resolve: {
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, 'app')
+    ],
+    extensions: ['.ts', '.js']
+  },
+
+  devtool: false
+};
