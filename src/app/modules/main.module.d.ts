f0baf6be0696f294304978a588b7d48b220102ac
blacksonic <soos.gabor86@gmail.com>
2017-02-22 19:48:29 +0100

    Updates

---
+++ src/app/modules/main.module.d.ts
@@ -0,0 +1,4 @@
+import { HelloWorldComponent } from '../components/hello-world.component';
+export { HelloWorldComponent };
+export declare class MainModule {
+}
---
+++ src/app/polyfill.d.ts
@@ -0,0 +1,4 @@
+import 'core-js/client/shim';
+import 'zone.js/dist/zone';
+import 'zone.js/dist/long-stack-trace-zone';
+import '../style.css';
